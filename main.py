87a984186ab637cf37612e69fe4f2ec5b0785142
EERON36 <aaron.sofonias@outlook.com>
2026-01-15 06:53:21 +0100

    add Player class and draw triangle ship in center

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -13,7 +17,12 @@ def main():
                 return
 
         screen.fill("black")
+
+        # draw player here
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
